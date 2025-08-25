dfdb642e277cd9627db8944b75ea475b59fbe54e
udittiwariut <udittiwariut@gmail.com>
2025-08-25 16:59:32 +0530

    completed with streams

--- helper.js
+++ helper.js
@@ -1,94 +1,62 @@
 import child_process from "child_process";
+import fs from "fs";
 import path from "path";
 import { CURSE_WORDS } from "./conts.js";
+import { createClient } from "@deepgram/sdk";
 
+export function extractAudio(videoFile) {
+  return child_process.spawn("ffmpeg", ["-i", videoFile, "-vn", "-f", "wav", "pipe:1"]).stdout;
+}
 
+export function addBeeps(videoFile, badSegments) {
+  let mainAudio = [];
+  let beep_creation = [];
+  let beep_play = [];
+  let beep_output = [];
 
+  badSegments.forEach(({ start, end }, i) => {
+    let duration = end - start;
+    mainAudio.push(`between(t,${start},${end})`);
+    beep_creation.push(`sine=frequency=1000:duration=${duration}[beep${i + 1}]`);
+    beep_play.push(`[beep${i + 1}]adelay=${start * 1000}|${start * 1000}[b${i + 1}]`);
+    beep_output.push(`[b${i + 1}]`);
+  });
 
-export function addBeeps(inputFile, outputFile, badSegments) {
-  return new Promise((resolve, reject) => {
-    let mainAudio = [];
-    let beep_creation = [];
-    let beep_play = [];
-    let beep_output = [];
-
-    badSegments.forEach(({ start, end }, i) => {
-      let duration = end - start;
-      mainAudio.push(`between(t,${start},${end})`);
-      beep_creation.push(`sine=frequency=1000:duration=${duration}[beep${i + 1}]`);
-      beep_play.push(`[beep${i + 1}]adelay=${start * 1000}|${start * 1000}[b${i + 1}]`);
-      beep_output.push(`[b${i + 1}]`);
-    });
-
-    const muteExpr = mainAudio.join("+");
-    const filterChain = `[0:a]volume=enable='${muteExpr}':volume=0[aud];
+  const muteExpr = mainAudio.join("+");
+  const filterChain = `[0:a]volume=enable='${muteExpr}':volume=0[aud];
                        ${beep_creation.join(";")};
                        ${beep_play.join(";")};
                        [aud]${beep_output.join("")}amix=inputs=${
-      beep_output.length + 1
-    }:duration=longest:normalize=0[out]`;
+    beep_output.length + 1
+  }:duration=longest:normalize=0[out]`;
 
-    const args = ["-y", "-i", inputFile, "-filter_complex", filterChain, "-map", "[out]", outputFile];
+  const args = ["-y", "-i", "pipe:0", "-filter_complex", filterChain, "-map", "[out]", "-f", "wav", "pipe:1"];
 
-    const ffmpegProcess = child_process.spawn("ffmpeg", args);
+  const ffmpegProcess = child_process.spawn("ffmpeg", args);
 
-    ffmpegProcess.on("close", (code) => {
-      if (code == 0) resolve(code);
-      else reject();
-    });
-  });
+  extractAudio(videoFile).pipe(ffmpegProcess.stdin);
+
+  return ffmpegProcess.stdout;
 }
 
-export function replaceAudio(videoFile, audioFile, outputFile) {
-  return new Promise((resolve, reject) => {
-    const ffmpeg = child_process.spawn("ffmpeg", [
-      "-i",
-      videoFile,
-      "-i",
-      audioFile,
-      "-c:v",
-      "copy",
-      "-map",
-      "0:v:0",
-      "-map",
-      "1:a:0",
-      "-shortest",
-      outputFile,
-    ]);
-
-    ffmpeg.on("close", (code) => {
-      if (code === 0) resolve(outputFile);
-      else reject(new Error("FFmpeg failed"));
-    });
-  });
-}
-
-export async function extractAudio(videoFile, audioFile) {
-  return new Promise((resolve, reject) => {
-    const ffmpeg = child_process.spawn("ffmpeg", ["-i", videoFile, "-vn", "-acodec", "copy", audioFile]);
-
-    ffmpeg.on("close", (code) => {
-      if (code === 0) resolve(outputFile);
-      else reject(new Error("FFmpeg failed"));
-    });
-  });
-}
-
-export async function transcribe(audioFile) {
+export async function transcribe(fileStream) {
   try {
-    const filePath = path.join(__dirname, audioFile);
-
     const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 
-    const fileStream = fs.createReadStream(path.resolve(__dirname, filePath));
-    const res = await deepgram.listen.prerecorded.transcribeFile(fileStream, {
-      model: "nova-3",
-      language: "en",
-    });
+    // const res = await deepgram.listen.prerecorded.transcribeFile(fileStream, {
+    //   model: "nova-3",
+    //   language: "en",
+    // });
+    // const jsonData = JSON.stringify(word, null, 2);
 
-    const jsonData = JSON.stringify(res, null, 2);
+    const res = fs.readFileSync("response.json", { encoding: "utf8" });
 
-    fs.writeFileSync("response.json", jsonData, "utf8");
+    // let word = res.result.results.channels[0].alternatives[0].words;
+
+    // fs.writeFileSync("", jsonData, "utf8");
+
+    return JSON.parse(res);
+    return word;
   } catch (error) {
     console.log(error.response);
   }
@@ -97,5 +65,64 @@ export async function transcribe(audioFile) {
 export function findBadSegments(segments) {
   return segments.filter((seg) => CURSE_WORDS.some((w) => seg.word.toLowerCase().includes(w)));
 }
+export function replaceAudio(videoFile, censored_audio_stream) {
+  const ffmpeg = child_process.spawn("ffmpeg", [
+    "-i",
+    videoFile,
+    "-i",
+    "pipe:0",
+    "-c:v",
+    "copy",
+    "-map",
+    "0:v:0",
+    "-map",
+    "1:a:0",
+    "-shortest",
+    "-movflags",
+    "frag_keyframe+empty_moov+default_base_moof",
+    "-f",
+    "mp4",
+    "pipe:1",
+  ]);
 
+  ffmpeg.stdin.on("error", (err) => {
+    console.error("stdin error:", err.message);
+  });
 
+  ffmpeg.stderr.on("data", (data) => {
+    console.error("ffmpeg stderr:", data.toString());
+  });
+
+  ffmpeg.on("close", (code) => {
+    console.log("ffmpeg exited with code", code);
+  });
+
+  censored_audio_stream.pipe(ffmpeg.stdin);
+
+  return ffmpeg.stdout;
+}
+
+export const pipeLine = async ({ videoFile }) => {
+  const pipe1 = extractAudio(videoFile);
+
+  const audio_response = await transcribe(pipe1);
+
+  const badSegments = findBadSegments(audio_response);
+
+  const censored_audio_stream = addBeeps(videoFile, badSegments);
+
+  const finalVideo = replaceAudio(videoFile, censored_audio_stream);
+
+  const output = fs.createWriteStream("output.mp4");
+  finalVideo.pipe(output);
+
+  return new Promise((resolve, reject) => {
+    output.on("finish", (message) => {
+      resolve();
+    });
+    output.on("error", (error) => {
+      console.log(error);
+      reject();
+    });
+  });
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import "dotenv/config";
-import { addBeeps, replaceAudio, extractAudio, transcribe, findBadSegments } from "./helper.js";
+import { pipeLine } from "./helper.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 // import busboy from "busboy";
@@ -14,25 +14,7 @@ global.__dirname = dirname(__filename);
   const censoredAudio = "censored_audio.mp3";
   const outputVideo = "output.mp4";
 
-  console.log("extracting audio...");
+  await pipeLine({ videoFile });
 
-  await extractAudio(videoFile, audioFile);
-
-  console.log("transcribe...");
-
-  await transcribe(audioFile);
-
-  const audio_response = JSON.parse(fs.readFileSync("./response.json", "utf-8"));
-
-  const words = audio_response.result.results.channels[0].alternatives[0].words;
-
-  const badSegments = findBadSegments(words);
-
-  console.log("adding beep...");
-
-  await addBeeps(audioFile, censoredAudio, badSegments);
-
-  console.log("replacing audio...");
-
-  await replaceAudio(videoFile, censoredAudio, outputVideo);
+  process.exit();
 })();
